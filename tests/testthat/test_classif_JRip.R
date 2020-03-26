d077a5ca95d1cfef2b68a279a97525698d8a5fde
HenriLMU <50552831+HenriLMU@users.noreply.github.com>
2020-03-26 10:37:24 +0100

    subuste

--- tests/testthat/test_classif_JRip.R
+++ tests/testthat/test_classif_JRip.R
@@ -2,7 +2,6 @@ context("classif.JRip")
 
 test_that("autotest", {
   learner = LearnerClassifJRip$new()
-  learner$param_set$values <- list(subset = 1:50)
   expect_learner(learner)
   result = run_autotest(learner)
   expect_true(result, info = result$error)
