039053a26b917987dd9e02b0a59adb87b5e40c23
HenriLMU <50552831+HenriLMU@users.noreply.github.com>
2020-03-31 09:30:28 +0200

    refresh cache

--- tests/testthat/test_classif_JRip.R
+++ tests/testthat/test_classif_JRip.R
@@ -1,5 +1,10 @@
 context("classif.JRip")
 
+skip_on_os("windows")
+skip_on_cran()
+
+requirePackagesOrSkip("RWeka", default.method = "load")
+
 test_that("autotest", {
   learner = LearnerClassifJRip$new()
   expect_learner(learner)
